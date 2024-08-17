74edc8cef7d3abca06e8eef1df5578f20a001e38
Owen Chen <owen.alex.chen@gmail.com>
2024-08-16 18:36:22 -0700

    core version - mostly bugfree???

---
+++ gmtk-2024-dartmouth/Assets/PlayerController.cs
@@ -0,0 +1,195 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 moveDirection;
+    [SerializeField] private float movementSpeed;
+
+    private bool isGrounded = false;
+
+    private bool jumpKeyDown = false;
+    [SerializeField] private float jumpVelocity;
+
+    private Rigidbody2D rb;
+
+    private GameObject sizeGun;
+    private Transform firePoint;
+
+    private Camera cam;
+    private Vector2 mouseWorldPos;
+
+    private bool leftClickDown;
+    private bool rightClickDown;
+
+    [SerializeField] private float storedSize;
+    [SerializeField] private float sizeDelta;
+
+    private Vector2 startingScale;
+
+    [SerializeField] private float playerMinSize;
+    [SerializeField] private float playerMaxSize;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+
+        sizeGun = transform.Find("Size Gun").gameObject;
+        firePoint = sizeGun.transform.Find("Fire Point");
+
+        startingScale = (Vector2) transform.localScale;
+    }
+
+    private void Update()
+    {
+        HandleMovementInput();
+        HandleAimingInput();
+        HandleShootingInput();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        HandleAiming();
+        HandleShooting();
+
+        transform.localScale = new Vector2(storedSize * startingScale.x, storedSize * startingScale.y);
+    }
+
+    private void HandleShootingInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            leftClickDown = true;
+
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            leftClickDown = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+           rightClickDown = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            rightClickDown = false;
+        }
+    }
+
+    private void HandleShooting() {
+        if (!(leftClickDown || rightClickDown))
+        {
+            return;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        if (hit.collider)
+        {
+            Debug.DrawLine(firePoint.position, hit.point, UnityEngine.Color.red);
+
+            SizeManager hitSizeManager = hit.transform.GetComponent<SizeManager>();
+            if (hitSizeManager != null)
+            {
+                if (leftClickDown)
+                {
+                    if (storedSize > 0)
+                    {
+                        float currentSize = hitSizeManager.GetSize();
+                        float maximumSize = hitSizeManager.GetMaxSize();
+
+                        if (storedSize - sizeDelta <= playerMinSize)
+                        {
+                            hitSizeManager.ChangeSize(storedSize - playerMinSize);
+                            storedSize = playerMinSize;
+                        }
+                        else if (currentSize + sizeDelta >= maximumSize)
+                        {
+                            storedSize -= maximumSize - currentSize;
+                            hitSizeManager.SetSize(maximumSize);
+                        }
+                        else
+                        {
+                            hitSizeManager.ChangeSize(sizeDelta);
+                            storedSize -= sizeDelta;
+                        }
+                    }
+                }
+                else if (rightClickDown)
+                {
+                    float currentSize = hitSizeManager.GetSize();
+                    float minimumSize = hitSizeManager.GetMinSize();
+
+                    if (storedSize + sizeDelta >= playerMaxSize)
+                    {
+                        hitSizeManager.ChangeSize(- 1 * storedSize + playerMaxSize);
+                        storedSize = playerMaxSize;
+                    }
+                    else if (currentSize - sizeDelta <= minimumSize)
+                    {
+                        storedSize += currentSize - minimumSize;
+                        hitSizeManager.SetSize(minimumSize);
+                    }
+                    else
+                    {
+                        hitSizeManager.ChangeSize(-1 * sizeDelta);
+                        storedSize += sizeDelta;
+
+                    }
+                }
+            }
+        }
+    }
+
+    private void HandleMovementInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        moveDirection = new Vector2(moveX, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            jumpKeyDown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.W))
+        {
+            jumpKeyDown = false;
+        }
+    }
+
+    private void HandleAimingInput()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        mouseWorldPos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+    }
+
+    private void HandleMovement()
+    {
+        rb.velocity = new Vector2(moveDirection.x * movementSpeed / storedSize, rb.velocity.y);
+
+        if (isGrounded && jumpKeyDown)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity / storedSize);
+        }
+    }
+
+    private void HandleAiming()
+    {
+        sizeGun.transform.right = (mouseWorldPos - (Vector2) transform.position).normalized;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        isGrounded = false;
+    }
+}
---
+++ gmtk-2024-dartmouth/Assets/SizeManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeManager : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private Vector2 startingScale;
+    [SerializeField] private float size;
+
+    [SerializeField] private float minimumSize;
+    [SerializeField] private float maximumSize;
+
+    [SerializeField] private bool scalesHorizontally;
+    [SerializeField] private bool scalesVertically;
+
+    [SerializeField] private bool isStatic;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        startingScale = (Vector2) transform.localScale;
+        transform.localScale *= size;
+
+        if (isStatic)
+        {
+            rb.bodyType = RigidbodyType2D.Static;
+        }
+        else
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+    private void Update()
+    {
+        if (scalesHorizontally && scalesVertically)
+        {
+            transform.localScale = new Vector2(size * startingScale.x, size * startingScale.y);
+        }
+        else if (scalesHorizontally)
+        {
+            transform.localScale = new Vector2(size * startingScale.x, startingScale.y);
+        }
+        else if (scalesVertically)
+        {
+            transform.localScale = new Vector2(startingScale.x, size * startingScale.y);
+        }
+    }
+
+    public float GetSize()
+    {
+        return size;
+    }
+
+    public void SetSize(float newSize)
+    {
+        size = newSize;
+    }
+
+    public void ChangeSize(float delta)
+    {
+        size += delta;
+    }
+
+    public float GetMinSize()
+    {
+        return minimumSize;
+    }
+
+    public float GetMaxSize()
+    {
+        return maximumSize;
+    }
+}
