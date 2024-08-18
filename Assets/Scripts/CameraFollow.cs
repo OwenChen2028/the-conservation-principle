215627f05a992eea64daa57e6d1aca83c34878b4
Owen Chen <owen.alex.chen@gmail.com>
2024-08-17 22:50:06 -0700

    preparing to merge

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,15 +7,14 @@ public class CameraFollow : MonoBehaviour
 	[SerializeField] private Transform followTransform;
 	[SerializeField] private float lerpConstant = 0.05f;
 
-	[Header("Directions to Follow")]
-	[SerializeField] private bool Vertical = true;
-	[SerializeField] private bool Horizontal = true;
+    [SerializeField] private bool horizontalScrolling = true;
+    [SerializeField] private bool verticalScrolling = true;
 
 	void FixedUpdate() 
 	{
 			Vector3 followPosition = new Vector3(
-				Horizontal ? followTransform.position.x : transform.position.x,
-				Vertical ? followTransform.position.y : transform.position.y,
+                horizontalScrolling ? followTransform.position.x : transform.position.x,
+                verticalScrolling ? followTransform.position.y : transform.position.y,
 				-10f
 			);
 			transform.position = Vector3.Lerp(transform.position, followPosition, lerpConstant);
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,216 +6,281 @@ using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
-	private Vector2 moveDirection;
+    private Vector2 moveDirection;
 
-	[SerializeField] private bool useOldMovement;
+    private Vector2 startingScale;
+    [SerializeField] private bool canScale;
 
-	[SerializeField] private float moveForce;
-	[SerializeField] private float movementSpeed;
-	
-	private bool isGrounded = false;
+    [SerializeField] private bool useOldMovement;
 
-	private bool jumpKeyDown = false;
-	[SerializeField] private float jumpVelocity;
+    [SerializeField] private float moveForce;
+    [SerializeField] private float movementSpeed;
+    
+    private bool isGrounded = false;
 
-	private Rigidbody2D rb;
+    private bool jumpKeyDown = false;
+    [SerializeField] private float jumpVelocity;
 
-	private GameObject sizeGun;
-	private Transform firePoint;
+    private Rigidbody2D rb;
 
-	private Camera cam;
-	private Vector2 mouseWorldPos;
+    private GameObject sizeGun;
+    private Transform firePoint;
 
-	private bool leftClickDown;
-	private bool rightClickDown;
+    private Camera cam;
+    private Vector2 mouseWorldPos;
 
-	[SerializeField] private float storedSize;
-	[SerializeField] private float sizeDelta;
+    private bool leftClickDown;
+    private bool rightClickDown;
 
-	private Vector2 startingScale;
+    [SerializeField] private float storedSize;
+    [SerializeField] private float sizeDelta;
 
-	[SerializeField] private float playerMinSize;
-	[SerializeField] private float playerMaxSize;
 
-	private void Awake()
-	{
-		// DO NOT INCLUDE IN BUILD
-		QualitySettings.vSyncCount = 0;  // VSync must be disabled for this to work
-		Application.targetFrameRate = 165;
-		
-		
-		rb = GetComponent<Rigidbody2D>();
-		cam = Camera.main;
+    [SerializeField] private float playerMinSize;
+    [SerializeField] private float playerMaxSize;
 
-		sizeGun = transform.Find("Size Gun").gameObject;
-		firePoint = sizeGun.transform.Find("Fire Point");
+    private Animator anim;
 
-		startingScale = (Vector2) transform.localScale;
-	}
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
 
-	private void Update()
-	{
-		HandleMovementInput();
-		HandleAimingInput();
-		HandleShootingInput();
-	}
+        sizeGun = transform.Find("Size Gun").gameObject;
+        firePoint = sizeGun.transform.Find("Fire Point");
 
-	private void FixedUpdate()
-	{
-		HandleMovement();
-		HandleAiming();
-		HandleShooting();
+        startingScale = new Vector2(1, 1);
 
-		// transform.localScale = new Vector2(storedSize * startingScale.x, storedSize * startingScale.y);
-	}
+        anim = GetComponent<Animator>();
+    }
 
-	private void HandleShootingInput()
-	{
-		if (Input.GetKeyDown(KeyCode.Mouse0))
-		{
-			leftClickDown = true;
+    private void Update()
+    {
+        HandleMovementInput();
+        HandleAimingInput();
+        HandleShootingInput();
+    }
 
-		}
-		if (Input.GetKeyUp(KeyCode.Mouse0))
-		{
-			leftClickDown = false;
-		}
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        HandleAiming();
+        HandleShooting();
 
-		if (Input.GetKeyDown(KeyCode.Mouse1))
-		{
-		   rightClickDown = true;
-		}
+        if (canScale)
+        {
+            transform.localScale = new Vector2(storedSize * startingScale.x, storedSize * startingScale.y);
+        }
+    }
 
-		if (Input.GetKeyUp(KeyCode.Mouse1))
-		{
-			rightClickDown = false;
-		}
-	}
+    private void HandleShootingInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            leftClickDown = true;
 
-	private void HandleShooting() {
-		if (!(leftClickDown || rightClickDown))
-		{
-			return;
-		}
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            leftClickDown = false;
+        }
 
-		RaycastHit2D hit = Physics2D.Raycast(firePoint.position, firePoint.right);
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+           rightClickDown = true;
+        }
 
-		if (hit.collider)
-		{
-			Debug.DrawLine(firePoint.position, hit.point, UnityEngine.Color.red);
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            rightClickDown = false;
+        }
+    }
 
-			SizeManager hitSizeManager = hit.transform.GetComponent<SizeManager>();
-			if (hitSizeManager != null)
-			{
-				if (leftClickDown)
-				{
-					if (storedSize > 0)
-					{
-						float currentSize = hitSizeManager.GetSize();
-						float maximumSize = hitSizeManager.GetMaxSize();
+    private void HandleMovementInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        moveDirection = new Vector2(moveX, 0);
 
-						if (storedSize - sizeDelta <= playerMinSize)
-						{
-							hitSizeManager.ChangeSize(storedSize - playerMinSize);
-							storedSize = playerMinSize;
-						}
-						else if (currentSize + sizeDelta >= maximumSize)
-						{
-							storedSize -= maximumSize - currentSize;
-							hitSizeManager.SetSize(maximumSize);
-						}
-						else
-						{
-							hitSizeManager.ChangeSize(sizeDelta);
-							storedSize -= sizeDelta;
-						}
-					}
-				}
-				else if (rightClickDown)
-				{
-					float currentSize = hitSizeManager.GetSize();
-					float minimumSize = hitSizeManager.GetMinSize();
+        if (moveDirection.x != 0)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else 
+        {
+            anim.SetBool("IsRunning", false);
+        }
 
-					if (storedSize + sizeDelta >= playerMaxSize)
-					{
-						hitSizeManager.ChangeSize(- 1 * storedSize + playerMaxSize);
-						storedSize = playerMaxSize;
-					}
-					else if (currentSize - sizeDelta <= minimumSize)
-					{
-						storedSize += currentSize - minimumSize;
-						hitSizeManager.SetSize(minimumSize);
-					}
-					else
-					{
-						hitSizeManager.ChangeSize(-1 * sizeDelta);
-						storedSize += sizeDelta;
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            jumpKeyDown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.W))
+        {
+            jumpKeyDown = false;
+        }
+    }
 
-					}
-				}
-			}
-		}
-	}
+    private void HandleAimingInput()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        mouseWorldPos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+    }
 
-	private void HandleMovementInput()
-	{
-		float moveX = Input.GetAxisRaw("Horizontal");
-		moveDirection = new Vector2(moveX, 0);
+    private void HandleMovement()
+    {
+        if (moveDirection.x < 0) {
+            transform.localScale = new Vector2(-1 * Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else if (moveDirection.x > 0) {
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
 
-		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
-		{
-			jumpKeyDown = true;
-		}
-		if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.W))
-		{
-			jumpKeyDown = false;
-		}
-	}
+        if (!canScale)
+        {
+            if (useOldMovement)
+            {
+                rb.velocity = new Vector2(moveDirection.x * movementSpeed, rb.velocity.y);
+            }
+            else
+            {
+                float maxSpeed = movementSpeed;
+                rb.AddForce(moveForce * moveDirection);
+                if (rb.velocity.x > maxSpeed)
+                {
+                    rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+                }
+                if (rb.velocity.x < -maxSpeed)
+                {
+                    rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+                }
+            }
 
-	private void HandleAimingInput()
-	{
-		Vector2 mousePos = Input.mousePosition;
-		mouseWorldPos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-	}
+            if (isGrounded && jumpKeyDown)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+                anim.SetTrigger("Jump");
+            }
+        }
+        else
+        {
+            if (useOldMovement)
+            {
+                rb.velocity = new Vector2(moveDirection.x * movementSpeed / storedSize, rb.velocity.y);
+            }
+            else
+            {
+                float maxSpeed = movementSpeed / storedSize;
+                rb.AddForce(moveForce * moveDirection);
+                if (rb.velocity.x > maxSpeed)
+                {
+                    rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+                }
+                if (rb.velocity.x < -maxSpeed)
+                {
+                    rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+                }
+            }
 
-	private void HandleMovement()
-	{
-		if (useOldMovement)
-		{
-			rb.velocity = new Vector2(moveDirection.x * movementSpeed / storedSize, rb.velocity.y);
-		}
-		else
-		{
-			float maxSpeed = movementSpeed;
-			rb.AddForce(moveForce * moveDirection);
-			if (rb.velocity.x > maxSpeed)
-			{
-				rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
-			}
-			if (rb.velocity.x < -maxSpeed)
-			{
-				rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
-			}
-		}
+            if (isGrounded && jumpKeyDown)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpVelocity / storedSize);
+                anim.SetTrigger("Jump");
+            }
+        }
+    }
 
-		if (isGrounded && jumpKeyDown)
-		{
-			rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
-		}
-	}
+    private void HandleAiming()
+    {
+        if (transform.localScale.x < 0) {
+            sizeGun.transform.right = -1 * (mouseWorldPos - (Vector2) transform.position).normalized;
+        }
+        else {
+            sizeGun.transform.right = (mouseWorldPos - (Vector2) transform.position).normalized;
+        }
+    }
 
-	private void HandleAiming()
-	{
-		sizeGun.transform.right = (mouseWorldPos - (Vector2) transform.position).normalized;
-	}
+    private void HandleShooting() {
+        if (!(leftClickDown || rightClickDown))
+        {
+            return;
+        }
 
-	private void OnTriggerEnter2D(Collider2D col)
-	{
-		isGrounded = true;
-	}
+        RaycastHit2D hit;
 
-	private void OnTriggerExit2D(Collider2D col)
-	{
-		isGrounded = false;
-	}
+        if (transform.localScale.x < 0) {
+            hit = Physics2D.Raycast(firePoint.position, -1 * firePoint.right);
+        }
+        else
+        {
+            hit = Physics2D.Raycast(firePoint.position, firePoint.right);
+        }
+
+        if (hit.collider)
+        {
+            Debug.DrawLine(firePoint.position, hit.point, UnityEngine.Color.red);
+
+            SizeManager hitSizeManager = hit.transform.GetComponent<SizeManager>();
+            if (hitSizeManager != null)
+            {
+                if (leftClickDown)
+                {
+                    if (storedSize > 0)
+                    {
+                        float currentSize = hitSizeManager.GetSize();
+                        float maximumSize = hitSizeManager.GetMaxSize();
+
+                        if (storedSize - sizeDelta <= playerMinSize)
+                        {
+                            hitSizeManager.ChangeSize(storedSize - playerMinSize);
+                            storedSize = playerMinSize;
+                        }
+                        else if (currentSize + sizeDelta >= maximumSize)
+                        {
+                            storedSize -= maximumSize - currentSize;
+                            hitSizeManager.SetSize(maximumSize);
+                        }
+                        else
+                        {
+                            hitSizeManager.ChangeSize(sizeDelta);
+                            storedSize -= sizeDelta;
+                        }
+                    }
+                }
+                else if (rightClickDown)
+                {
+                    float currentSize = hitSizeManager.GetSize();
+                    float minimumSize = hitSizeManager.GetMinSize();
+
+                    if (storedSize + sizeDelta >= playerMaxSize)
+                    {
+                        hitSizeManager.ChangeSize(- 1 * storedSize + playerMaxSize);
+                        storedSize = playerMaxSize;
+                    }
+                    else if (currentSize - sizeDelta <= minimumSize)
+                    {
+                        storedSize += currentSize - minimumSize;
+                        hitSizeManager.SetSize(minimumSize);
+                    }
+                    else
+                    {
+                        hitSizeManager.ChangeSize(-1 * sizeDelta);
+                        storedSize += sizeDelta;
+
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        isGrounded = true;
+        anim.SetBool("IsGrounded", true);
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        isGrounded = false;
+        anim.SetBool("IsGrounded", false);
+    }
 }
