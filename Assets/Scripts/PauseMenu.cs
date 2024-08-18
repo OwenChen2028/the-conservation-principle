eb68a4d24b58aad703aa4e273ea875103243718d
Owen Chen <owen.alex.chen@gmail.com>
2024-08-18 11:03:01 -0700

    tweaked the visuals a bit (including bg tint)

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -10,7 +10,7 @@ public class PauseMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)) {
             if (!paused)
             {
                 PauseGame();
