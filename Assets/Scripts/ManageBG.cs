55f47a9a15a4d39a802376ae02b54aab99bd81c3
Tanner Hume <tanner.j.hume.28@dartmouth.edu>
2024-08-17 23:18:08 -0400

    Added Dynamic Background
    Added a background that rotates around a fixed point and slowly pulses. The background is a prefab that I did NOT add to the levels so just add the background manually.

---
+++ Assets/Scripts/ManageBG.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageBG : MonoBehaviour
+{
+
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private Vector2 pivotPoint;
+    [SerializeField] private float pulseFreq;
+    [SerializeField] private float minBrightness;
+
+    private Transform tran;
+    private SpriteRenderer rend;
+
+    private void Awake()
+    {
+        tran = GetComponent<Transform>();
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        tran.RotateAround(new Vector3(pivotPoint.x, pivotPoint.y, transform.position.z), Vector3.forward, rotateSpeed);
+
+        float brightness = oscillate(Time.timeSinceLevelLoad, pulseFreq, minBrightness, 1);
+        rend.color = new Color(brightness, brightness, brightness, 1);
+    }
+
+    private float oscillate(float time, float speed, float min, float max)
+    {
+        return Mathf.Lerp(min, max, (Mathf.Cos(time * speed / Mathf.PI) + 1f)*0.5f);
+    }
+}
