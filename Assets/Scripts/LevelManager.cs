c93725fdf556a711468b73132c144d5bde35631a
Owen Chen <owen.alex.chen@gmail.com>
2024-08-18 14:31:33 -0700

    laser bugfix

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    private static string[] hints = {""};
+}
