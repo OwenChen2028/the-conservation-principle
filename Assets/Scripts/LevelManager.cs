760e624deb7737ea3497a228ad0edf0e6fd7391a
Owen Chen <owen.alex.chen@gmail.com>
2024-08-19 11:09:54 -0700

    level B1 & B2 integration

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -12,17 +12,21 @@ public class LevelManager : MonoBehaviour
         "LevelA3",
         "LevelA4",
         "LevelA5",
-        "LevelA6"
+        "LevelA6",
+        "LevelB1",
+        "LevelB2",
     };
 
     public static string[] hints = {
-        "Right click to shrink objects, left click to enlarge them.", 
+        "Stand on objects while enlarging them to gain height.", 
         "Shrinking objects can make them fit into smaller gaps.",
         "You can take mass from one object to give it to another.",
         "Enlarging objects can change their center of mass.",
         "Enlarging objects pushes them away from walls.",
-        "Take all the mass you can get, leave nothing behind!"
+        "Take all your mass with you, leave nothing behind.",
+        "You can use other objects to hold buttons in place.",
+        "Enlarging an object can make it easier to reach."
     };
 
-    public static bool[] completed = new bool[6]; // defaults to false
+    public static bool[] completed = new bool[levels.Length]; // defaults to false
 }
